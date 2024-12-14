577b40f2d0784c5e368067dae17e4d792f59d9e6
ravishankar0908 <ravishankar007cbi@gmail.com>
2024-12-14 12:44:12 +0530

    connected the server with mongoDb using mongoose.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import RegisterRouter from "./routes/auth.routes.js";
 const port = 3000;
 const app = express();
 
 // MiddleWares
+dotenv.config();
 app.use('/auth', RegisterRouter);
 
 
 
+// connecting the server with the database
+const connection = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to the Database.");
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 // make the server start and listen on the port.
 app.listen(port, ()=>{
-    console.log(`The server is listening on http://localhost:${port}`);
+    connection();
+    console.log(`Connected to the server, listening on http://localhost:${port}`);
 });
